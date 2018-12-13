c98636d2370ecb8af7aa729e541c9223559d31e4
Wilson <wilson.dk713@gmail.com>
2018-12-12 22:20:44 -0500

    Rutas navegacion

--- angular/appAngular/src/app/app.module.ts
+++ angular/appAngular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/appAngular/src/app/usuario/usuario.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UsuarioService } from './usuario.service';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css'],
+  providers:[UsuarioService]
+})
+export class UsuarioComponent implements OnInit {
+
+  constructor(
+    private usuarioServicio: UsuarioService
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
